d2866f23c9bb0b48f00f17399e6d5ff18c399486
Paweł <84644073+Pawel-Lechki@users.noreply.github.com>
2023-08-13 21:15:45 +0200

    add signup function

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,9 +1,35 @@
 import {Link} from "react-router-dom";
+import {useRef} from "react";
+import axiosClient from "../axios-client.jsx";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
 
 export default function Signup() {
+  const nameRef = useRef()
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const passwordConfirmationRef = useRef()
+
+  const {setUser, setToken} = useStateContext()
 
   const onSubmit = (ev) => {
     ev.preventDefault()
+    const payload = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      password_confirmation: passwordConfirmationRef.current.value,
+    }
+    axiosClient.post('/signup', payload)
+      .then(({data}) => {
+        setUser(data.user)
+        setToken(data.token)
+      })
+      .catch(err => {
+        const response = err.response
+        if(response && response.status === 422) {
+          console.log(response.data.errors)
+        }
+      })
   }
 
 
@@ -12,10 +38,10 @@ export default function Signup() {
         <div className="form">
           <form onSubmit={onSubmit}>
             <h1 className="title">Signup for free</h1>
-            <input type="text" placeholder="Full name"/>
-            <input type="email" placeholder="Email Adtes"/>
-            <input type="password" placeholder="Password"/>
-            <input type="password" placeholder="Password Confirmation"/>
+            <input ref={nameRef} type="text" placeholder="Full name"/>
+            <input ref={emailRef} type="email" placeholder="Email Adtes"/>
+            <input ref={passwordRef} type="password" placeholder="Password"/>
+            <input ref={passwordConfirmationRef} type="password" placeholder="Password Confirmation"/>
             <button className="btn btn-block">Signun</button>
             <p className="message">
               Already Registered? <Link to="/login">Sign in</Link>
